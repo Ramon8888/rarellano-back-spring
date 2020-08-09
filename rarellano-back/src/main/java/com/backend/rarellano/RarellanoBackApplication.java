14403e74106471a36d5b86aa1b6314142eccaf09
Ramon Arellano Castro <ramo.arellano@infotec.mx>
2020-08-09 16:00:55 -0500

    Carga de archivos iniciales

---
+++ rarellano-back/src/main/java/com/backend/rarellano/RarellanoBackApplication.java
@@ -0,0 +1,13 @@
+package com.backend.rarellano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RarellanoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RarellanoBackApplication.class, args);
+	}
+
+}
---
+++ rarellano-back/src/test/java/com/backend/rarellano/RarellanoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rarellano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RarellanoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
